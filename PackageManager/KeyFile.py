a7c0fe322129bb6782652639719185678ec8a141
David James Lario <dlario@ualberta.net>
2024-02-03 22:09:43 -0700

    First Load

---
+++ PackageManager/KeyFile.py
@@ -0,0 +1,13 @@
+def getKeys():
+    keyDict = {}
+    keyDict["WhatsApp"] = {"WhatsaAppToken": '',
+                "WhatsAppNumber": '',
+                "FB_VERIFY_TOKEN": ""}
+
+    keyDict["NOMIC"] = {"NOMICKEY": ""}
+
+    keyDict["OpenAI"] = {"OpenAIKey": "",
+                "OpenAIOrg": ""}
+
+    keyDict["MySQL"] = {"Login": "", "Password": ""}
+    return keyDict
